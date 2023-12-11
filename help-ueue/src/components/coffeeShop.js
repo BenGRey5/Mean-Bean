479acd43440f088d62c8a8cb368d46a9ee202e0a
BenGRey5 <146392527+BenGRey5@users.noreply.github.com>
2023-12-11 06:24:29 -0800

    Update coffeeShop.js

--- help-ueue/src/components/coffeeShop.js
+++ help-ueue/src/components/coffeeShop.js
@@ -1,14 +1,11 @@
-import React from 'react';
-
-function Item({ item, onBuy, onRestock, onDelete}) {
+function Item({ item, onSell, onDelete }) {
     return (
         <div>
             <h2>{item.name}</h2>
             <p>{item.description}</p>
-            <p>{item.quantity > 0 ? `Quantity: ${item.quantity}` : 'Out of Stock'}</p>
-<button onClick={() => onBuy(item.id)}>Buy</button>
-<button onClick={() => onRestock(item.id, 1)}>Restock by 1</button>
-<button onClick={() => onDelete(item.id)}>Delete</button>
+            <p>{item.quantity > 0 ? `Quantity: ${item.quantity}` : 'Bag empty'}</p>
+            <button onClick={() => onSell(item.id)}>Sell</button>
+            <button onClick={() => onDelete(item.id)}>Delete</button>
         </div>
     );
 }
